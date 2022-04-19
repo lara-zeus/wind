91f6883ab4d3d40360817cda8be99cfc762952a9
Ashraf Monshi <wh7r.com@gmail.com>
2022-04-19 09:22:31 +0300

    fix string concat in config

--- config/zeus-wind.php
+++ config/zeus-wind.php
@@ -39,8 +39,8 @@ return [
     /**
      * this will be setup the default seo site title. read more about it in 'laravel-seo'
      */
-    'site_title' => config('app.name', 'Laravel').' | '.'Contact Us',
-
+    'site_title' => config('app.name', 'Laravel').' | Contact Us',
+    
     /**
      * this will be setup the default seo site description. read more about it in 'laravel-seo'
      */
