4d255bcf2a9646ee8cf0b62374bcad24021f32b5
Ashraf Monshi <wh7r.com@gmail.com>
2022-06-21 22:21:38 +0300

    allow to embed the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,16 +7,5 @@
         {{ __('feel free to contact us.') }}
     </x-zeus::box>
 
-    @if($sent)
-        @include('zeus-wind::submitted')
-    @else
-        <form wire:submit.prevent="store">
-            <div class="max-w-4xl mx-auto my-4">
-                {{ $this->form }}
-                <div class="p-4 text-center">
-                    <x-zeus::button type="submit">{{ __('Send') }}</x-zeus::button>
-                </div>
-            </div>
-        </form>
-    @endif
+    <livewire:contact-form :departmentSlug="$departmentSlug" />
 </div>
