0bff903f6e63958522cdb3f290dcbb2a4e4fbd38
Ash Monsh <wh7r.com@gmail.com>
2025-04-07 10:23:46 +0300

    support filament 4

--- src/Filament/Resources/DepartmentResource.php
+++ src/Filament/Resources/DepartmentResource.php
@@ -6,19 +6,19 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
-use Filament\Forms\Form;
-use Filament\Forms\Set;
 use Filament\Resources\Resource;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\ActionGroup;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ForceDeleteAction;
-use Filament\Tables\Actions\ForceDeleteBulkAction;
-use Filament\Tables\Actions\RestoreAction;
-use Filament\Tables\Actions\RestoreBulkAction;
-use Filament\Tables\Actions\ViewAction;
+use Filament\Schemas\Components\Utilities\Set;
+use Filament\Schemas\Schema;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
@@ -34,7 +34,7 @@ use LaraZeus\Wind\WindPlugin;
 
 class DepartmentResource extends Resource
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-duplicate';
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-document-duplicate';
 
     protected static ?int $navigationSort = 1;
 
@@ -48,9 +48,9 @@ class DepartmentResource extends Resource
         return static::getModel()::count();
     }
 
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return $form
+        return $schema
             ->schema([
                 TextInput::make('name')
                     ->required()
--- src/Filament/Resources/LetterResource.php
+++ src/Filament/Resources/LetterResource.php
@@ -4,19 +4,20 @@ namespace LaraZeus\Wind\Filament\Resources;
 
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
-use Filament\Tables\Actions\ActionGroup;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ForceDeleteAction;
-use Filament\Tables\Actions\ForceDeleteBulkAction;
-use Filament\Tables\Actions\RestoreAction;
-use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\Layout\Split;
 use Filament\Tables\Columns\Layout\Stack;
@@ -33,7 +34,7 @@ use LaraZeus\Wind\WindPlugin;
 
 class LetterResource extends Resource
 {
-    protected static ?string $navigationIcon = 'heroicon-o-inbox';
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-inbox';
 
     protected static ?int $navigationSort = 2;
 
@@ -58,14 +59,15 @@ class LetterResource extends Resource
             ]);
     }
 
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return $form
+        return $schema
             ->schema([
                 Section::make()
+                    ->columnSpanFull()
                     ->visibleOn('edit')
                     ->schema([
-                        Placeholder::make('sender_info')
+                        TextEntry::make('sender_info')
                             ->label('Sender Info:')
                             ->columnSpan(['sm' => 2]),
 
@@ -92,15 +94,16 @@ class LetterResource extends Resource
                             ->label(__('sent at'))
                             ->disabled(),
 
-                        Placeholder::make('message')
+                        TextEntry::make('message')
                             ->label(__('message'))
                             ->disabled()
-                            ->content(fn (Letter $record) => new HtmlString($record->message))
+                            ->state(fn (Letter $record) => new HtmlString($record->message))
                             ->columnSpan(['sm' => 2]),
                     ])
                     ->columns(),
 
                 Section::make()
+                    ->columnSpanFull()
                     ->visibleOn('edit')
                     ->schema([
                         Select::make('department_id')
